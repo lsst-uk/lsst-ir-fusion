64969d0c71deb395aad99707e1a0c1fdb912c84f
Raphael Shirley <Raphael.Shirley@sussex.ac.uk>
2020-08-03 18:27:12 +0200

    Try to ingest all of VIDEO

---
+++ dmu4/dmu4_XMM-LSS/1_Ingest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# source /home/ir-shir1/lsst_stack/loadLSST.bash
+source /rfs/project/rfs-L33A9wsNuJk/shared/lsst_stack/loadLSST.bash
+setup lsst_distrib
+ingestImages.py data /home/ir-shir1/rds/rds-iris-ip005/data/private/VISTA/VIDEO/**/*_st.fit 
