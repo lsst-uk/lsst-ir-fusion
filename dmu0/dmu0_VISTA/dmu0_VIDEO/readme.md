b743d282e85e20f9fa4d4a67ff68fca9875cef91
Raphael Shirley <Raphael.Shirley@sussex.ac.uk>
2020-08-19 19:53:47 +0200

    Add readme for VIDEO
    Making code run via links here instead of yml file to describe file locations

---
+++ dmu0/dmu0_VISTA/dmu0_VIDEO/readme.md
@@ -0,0 +1,3 @@
+# VISTA VIDEO
+
+This folder stores images and catalogues from the official VIDEO data release
--- dmu4/dmu4_XMM-LSS/1_Ingest.sh
+++ dmu4/dmu4_XMM-LSS/1_Ingest.sh
@@ -2,4 +2,5 @@
 # source /home/ir-shir1/lsst_stack/loadLSST.bash
 source /rfs/project/rfs-L33A9wsNuJk/shared/lsst_stack/loadLSST.bash
 setup lsst_distrib
+setup obs_vista
 ingestImages.py data /home/ir-shir1/rds/rds-iris-ip005/data/private/VISTA/VIDEO/**/*_st.fit 
