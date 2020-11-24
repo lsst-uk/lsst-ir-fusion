c8639e05478a01bb242409a42cff8761a8fe23fc
Raphael Shirley <raphaelshirley@gmail.com>
2020-11-24 13:57:45 +0000

    Slurm tinkering

--- dmu4/dmu4_XMM/1_Ingest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# source /home/ir-shir1/lsst_stack/loadLSST.bash
-source /rfs/project/rfs-L33A9wsNuJk/shared/lsst_stack/loadLSST.bash
-setup lsst_distrib
-setup obs_vista
-ingestImages.py data /home/ir-shir1/rds/rds-iris-ip005/data/private/VISTA/VHS/**/*_st.fit 
