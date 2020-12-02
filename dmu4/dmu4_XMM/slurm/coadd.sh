732747dc7fea9d8622a4213df5ebb1b7ae2e67dd
Raphael Shirley <raphaelshirley@gmail.com>
2020-12-02 15:41:20 +0000

    Bug in coadd fixed

--- dmu4/dmu4_XMM/slurm/coadd.sh
+++ dmu4/dmu4_XMM/slurm/coadd.sh
@@ -15,22 +15,22 @@ echo $tract
 echo $patch
 echo $numCPUs
 
-for f in ../data/rerun/coadd/deepCoadd/VISTA-Y/$tract/$patch/*; do export Y_VISITS=$Y_VISITS^${{f:${{#f}}-23:6}}; done
-export Y_VISITS=${{Y_VISITS//-}}
-export Y_VISITS=${{Y_VISITS//^/' --selectId filter=VISTA-Y visit='}}
+for f in ../data/rerun/coadd/deepCoadd/VISTA-Y/$tract/$patch/*; do export Y_VISITS=$Y_VISITS^${f:${#f}-23:6}; done
+export Y_VISITS=${Y_VISITS//-}
+export Y_VISITS=${Y_VISITS//^/' --selectId filter=VISTA-Y visit='}
 assembleCoadd.py ../data --rerun coadd $Y_VISITS --id filter=VISTA-Y tract=$tract patch=$patch -j=$numCPUs
 
-for f in ../data/rerun/coadd/deepCoadd/VISTA-J/$tract/$patch/*; do export J_VISITS=$J_VISITS^${{f:${{#f}}-23:6}}; done
-export J_VISITS=${{J_VISITS//-}}
-export J_VISITS=${{J_VISITS//^/' --selectId filter=VISTA-J visit='}}
+for f in ../data/rerun/coadd/deepCoadd/VISTA-J/$tract/$patch/*; do export J_VISITS=$J_VISITS^${f:${#f}-23:6}; done
+export J_VISITS=${J_VISITS//-}
+export J_VISITS=${J_VISITS//^/' --selectId filter=VISTA-J visit='}
 assembleCoadd.py ../data --rerun coadd $J_VISITS --id filter=VISTA-J tract=$tract patch=$patch -j=$numCPUs
 
-for f in ../data/rerun/coadd/deepCoadd/VISTA-H/$tract/$patch/*; do export H_VISITS=$H_VISITS^${{f:${{#f}}-23:6}}; done
-export H_VISITS=${{H_VISITS//-}}
-export H_VISITS=${{H_VISITS//^/' --selectId filter=VISTA-H visit='}}
+for f in ../data/rerun/coadd/deepCoadd/VISTA-H/$tract/$patch/*; do export H_VISITS=$H_VISITS^${f:${#f}-23:6}; done
+export H_VISITS=${H_VISITS//-}
+export H_VISITS=${H_VISITS//^/' --selectId filter=VISTA-H visit='}
 assembleCoadd.py ../data --rerun coadd $H_VISITS --id filter=VISTA-H tract=$tract patch=$patch -j=$numCPUs
 
-for f in ../data/rerun/coadd/deepCoadd/VISTA-Ks/$tract/$patch/*; do export KS_VISITS=$KS_VISITS^${{f:${{#f}}-23:6}}; done
-export KS_VISITS=${{KS_VISITS//-}}
-export KS_VISITS=${{KS_VISITS//^/' --selectId filter=VISTA-Ks visit='}}
+for f in ../data/rerun/coadd/deepCoadd/VISTA-Ks/$tract/$patch/*; do export KS_VISITS=$KS_VISITS^${f:${#f}-23:6}; done
+export KS_VISITS=${KS_VISITS//-}
+export KS_VISITS=${KS_VISITS//^/' --selectId filter=VISTA-Ks visit='}
 assembleCoadd.py ../data --rerun coadd $KS_VISITS --id filter=VISTA-Ks tract=$tract patch=$patch -j=$numCPUs
