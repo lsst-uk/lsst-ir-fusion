5286c1220498ffcf345e5a19a8e20bffe69c4aed
Raphael Shirley <Raphael.Shirley@sussex.ac.uk>
2021-01-11 10:37:11 +0000

    Make reduced cat slurm

---
+++ dmu5/dmu5_SXDS/slurm/1_Make_cat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# source /home/ir-shir1/lsst_stack/loadLSST.bash
+source /rfs/project/rfs-L33A9wsNuJk/shared/lsst_stack/loadLSST.bash
+setup lsst_distrib
+/usr/bin/time -o trace -a -f '%C %M kb %e s' jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=-1 ../1_Make_cat.ipynb --ExecutePreprocessor.kernel_name=lsst
