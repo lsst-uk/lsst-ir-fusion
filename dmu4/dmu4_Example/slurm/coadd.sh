16e211a29c55da8781904dd487d616ab5f46f6f5
Raphael Shirley <raphaelshirley@gmail.com>
2021-01-12 11:15:58 +0000

    minor slurm updates

--- dmu4/dmu4_Example/slurm/coadd.sh
+++ dmu4/dmu4_Example/slurm/coadd.sh
@@ -15,6 +15,11 @@ echo $tract
 echo $patch
 echo $numCPUs
 
+for f in ../data/rerun/coadd/deepCoadd/VISTA-Z/$tract/$patch/*; do export Z_VISITS=$Z_VISITS^${f:${#f}-23:6}; done
+export Z_VISITS=${Z_VISITS//-}
+export Z_VISITS=${Z_VISITS//^/' --selectId filter=VISTA-Z visit='}
+assembleCoadd.py ../data --rerun coadd $Z_VISITS --id filter=VISTA-Z tract=$tract patch=$patch -j=$numCPUs
+
 for f in ../data/rerun/coadd/deepCoadd/VISTA-Y/$tract/$patch/*; do export Y_VISITS=$Y_VISITS^${f:${#f}-23:6}; done
 export Y_VISITS=${Y_VISITS//-}
 export Y_VISITS=${Y_VISITS//^/' --selectId filter=VISTA-Y visit='}
