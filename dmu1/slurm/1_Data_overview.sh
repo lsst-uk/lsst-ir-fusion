c658071b317f30a1128f05e4e771fddd83c86f02
Raphael Shirley <raphaelshirley@gmail.com>
2021-04-14 11:31:14 +0100

    Work on VIKING Slurm files

--- dmu1/slurm/1_Data_overview.sh
+++ dmu1/slurm/1_Data_overview.sh
@@ -2,4 +2,4 @@
 # source /home/ir-shir1/lsst_stack/loadLSST.bash
 source /rfs/project/rfs-L33A9wsNuJk/shared/lsst_stack/loadLSST.bash
 setup lsst_distrib
-/usr/bin/time -o trace -a -f '%C %M kb %e s' jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=-1 ../1_Data_overview.ipynb --output 1_Data_overview-out.ipynb --ExecutePreprocessor.kernel_name=lsst
+/usr/bin/time -o trace -a -f '%C %M kb %e s' jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=-1 ../1_Data_overview.ipynb --ExecutePreprocessor.kernel_name=lsst
