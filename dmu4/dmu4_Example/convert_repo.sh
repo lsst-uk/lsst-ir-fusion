996c0b6d1c35dac67218b56536f1e89db0a8889c
Raphael Shirley <raphael.shirley@hotmail.co.uk>
2021-06-09 13:06:43 +0100

    Figures added

--- dmu4/dmu4_Example/convert_repo.sh
+++ dmu4/dmu4_Example/convert_repo.sh
@@ -3,5 +3,5 @@ source /Users/raphaelshirley/Documents/github/lsst_stack/loadLSST.bash
 setup lsst_distrib
 setup obs_vista 21.0.0-1
 
-butler convert --help
+butler convert --gen2root data_g2 data_g3_test
 
