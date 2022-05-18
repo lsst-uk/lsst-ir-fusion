f1efa4eb07330446a972e1b1bbe1a597d8e9b3e9
Raphael Shirley <raphael.shirley@hotmail.co.uk>
2022-05-18 16:56:32 +0100

    SQL for HSC SXDS test cats

--- dmu0/dmu0_2MASS/readme.md
+++ dmu0/dmu0_2MASS/readme.md
@@ -2,10 +2,23 @@
 
 This product contains the 2MASS All-Sky Catalogue of Point Sources (Cutri et al., 2003). Its full documentation is available here: http://cdsarc.u-strasbg.fr/viz-bin/Cat?II/246
 
+The all HSC coverage is downloaded from 
+
+http://tapvizier.u-strasbg.fr/adql/?%20II/246/out
+
+using the following query:
+
+``` sql
+ SELECT "II/246/out".RAJ2000,  "II/246/out".DEJ2000,  "II/246/out"."2MASS",  "II/246/out".Jmag,  "II/246/out".e_Jmag,  "II/246/out".Hmag,  "II/246/out".e_Hmag, 
+ "II/246/out".Kmag,  "II/246/out".e_Kmag,  "II/246/out".Qflg,  "II/246/out".Rflg,  "II/246/out".Bflg,  "II/246/out".Cflg,  "II/246/out".Xflg,  "II/246/out".Aflg
+ FROM "II/246/out"
+WHERE ("II/246/out".DEJ2000 > -15 AND "II/246/out".DEJ2000 < 3)
+```
+
 The final all sky point source catalog was downloaded from:
 
 https://irsa.ipac.caltech.edu/cgi-bin/Gator/nph-query
 
 No restraints were put on the query.
 
-We make 2MASS photometric reference cataloguyes as a test but they are superseeded by bootstrapped VISTA reference catalogeus which are themselves cal;ibrated against 2MASS. See [dmu2](../../dmu2/) for more information.
+[//]: # (We make 2MASS photometric reference catalogues as a test but they are superseeded by bootstrapped VISTA reference catalogeus which are themselves cal;ibrated against 2MASS. See [dmu2](../../dmu2/) for more information.)
--- dmu4/dmu4_Example/test_patch_gen3.sh
+++ dmu4/dmu4_Example/test_patch_gen3.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #source /home/ir-shir1/rds/rds-iris-ip005/ras81/lsst_stack/loadLSST.bash
-source /Users/raphaelshirley/Documents/github/lsst_stack/loadLSST.bash
-setup obs_vista 23.0.0-1
-setup lsst_distrib -t w_latest
+#source /Users/raphaelshirley/Documents/github/lsst_stack/loadLSST.bash
+#setup obs_vista 23.0.0-1
+#setup lsst_distrib -t w_2022_07
+source /Users/raphaelshirley/Documents/github/lsst-ir-fusion/setup_mac.sh
 
 export repo=data
 
@@ -20,8 +21,10 @@ if [ ! -f $repo/butler.yaml ]; then
     
     # Import the reference catalogues to the butler.
     butler register-dataset-type $repo ps1_pv3_3pi_20170110_vista SimpleCatalog htm7
-    cp -r ../../dmu2/data/video_gen3 $repo 
+    cp -r ../../dmu2/data/video_gen3 $repo  # VIDEO ref
     butler ingest-files -t direct $repo ps1_pv3_3pi_20170110_vista refcats data/video_gen3/filename_to_htm.ecsv 
+    #cp -r ../../dmu2/data/g3_2mass $repo  # 2MASS ref
+    #butler ingest-files -t direct $repo ps1_pv3_3pi_20170110_vista refcats data/g3_2mass/filename_to_htm.ecsv 
     
     #Ingest the raw exposures _st for stacks [0-9] for exposures
     butler ingest-raws $repo ../../dmu0/dmu0_VISTA/dmu0_VIDEO/data/*/*_st.fit -t copy
@@ -37,20 +40,22 @@ fi
 butler register-dataset-type $repo confidence ExposureF instrument band physical_filter exposure detector
 butler ingest-files --formatter=lsst.obs.vista.VircamRawFormatter $repo confidence videoConfidence ../../dmu0/dmu0_VISTA/dmu0_VIDEO/test_export_confidence.ecsv -t copy
 
+#pipetask run -d "exposure=658653 AND detector=10" -b $repo --input videoConfidence,VIRCAM/raw/all,refcats,VIRCAM/calib --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#singleFrame" --output videoSingleFrame  
+
 #Run processCcd on some exposures singleFrame processCcd
-pipetask run -d "detector IN (10,11) AND band IN ('J','K')" -b $repo --input videoConfidence,VIRCAM/raw/all,refcats,VIRCAM/calib --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#singleFrame" --output videoSingleFrame   #--extend-run --clobber-outputs --skip-existing
+pipetask run -d "detector IN (9,10) AND band IN ('J','K')" -b $repo --input videoConfidence,VIRCAM/raw/all,refcats,VIRCAM/calib --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#singleFrame" --output videoSingleFrame   #--extend-run --clobber-outputs --skip-existing
 
 #Coadd the exposures
 pipetask run -d "tract=8524 AND patch IN (39,48) AND skymap='hscPdr2' " -b $repo --input videoConfidence,skymaps,VIRCAM/raw/all,refcats,VIRCAM/calib,videoSingleFrame  --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#coaddDetect" --output videoCoaddDetect
 
-#Import HSC images
-#butler import $repo "../../dmu0/dmu0_HSC" --export-file "../../dmu0/dmu0_HSC/export.yaml" 
-coaddRun=$(ls $repo/videoCoaddDetect)
+#Import HSC deepCoadd images and detections
+#These must be ingested into a RUN collection so we ls it from the coadd collection 
+#coaddRun=$(ls $repo/videoCoaddDetect)
+coaddRun=hscImports
 butler ingest-files --formatter=lsst.obs.base.formatters.fitsExposure.FitsExposureFormatter $repo deepCoadd videoCoaddDetect/$coaddRun ../../dmu0/dmu0_HSC/data/calexp_2.ecsv
-coaddRun=$(ls $repo/videoCoaddDetect)
 butler ingest-files --formatter=lsst.obs.base.formatters.fitsExposure.FitsExposureFormatter $repo deepCoadd_calexp videoCoaddDetect/$coaddRun ../../dmu0/dmu0_HSC/data/calexp_2.ecsv
 butler ingest-files --formatter=lsst.obs.base.formatters.fitsGeneric.FitsGenericFormatter $repo deepCoadd_det videoCoaddDetect/$coaddRun ../../dmu0/dmu0_HSC/data/det_2.ecsv
 
 #Run photometry pipeline
-pipetask run -d "tract=8524 AND patch IN (39,48) AND skymap='hscPdr2' " -b $repo --input videoConfidence,VIRCAM/raw/all,refcats,VIRCAM/calib,skymaps,videoSingleFrame,videoCoaddDetect --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#multiVisitLater" --output videoMultiVisitLater
+pipetask run -d "tract=8524 AND patch IN (39,48) AND skymap='hscPdr2' " -b $repo --input videoConfidence,VIRCAM/raw/all,refcats,VIRCAM/calib,skymaps,videoSingleFrame,videoCoaddDetect,videoCoaddDetect/$coaddRun --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#multiVisitLater" --output videoMultiVisitLater
 
