cbcae10086ce4ae417942e911f2452bc847e3a7d
Raphael Shirley <raphaelshirley@gmail.com>
2022-05-25 16:58:21 +0100

    Switching VIDEO stages 0 and 1 to gen 3

--- dmu4/dmu4_VIDEO/slurm/ingest.sh
+++ dmu4/dmu4_VIDEO/slurm/ingest.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-source /rfs/project/rfs-L33A9wsNuJk/shared/lsst_stack_v21/loadLSST.bash
+#source /rfs/project/rfs-L33A9wsNuJk/shared/stack23/loadLSST.bash
+source /home/ir-shir1/rds/rds-iris-ip005/ras81/stack23/loadLSST.bash
 setup lsst_distrib
 setup obs_vista
 eups admin clearLocks
+
+export NUMEXPR_MAX_THREADS=32
+
+#butler ingest-raws ../../data /home/ir-shir1/rds/rds-iris-ip005/data/private/VISTA/VIDEO/*/*st.fit -t copy -j 32
+export repo=../../data
+butler register-dataset-type $repo confidence ExposureF instrument band physical_filter exposure detector
 
 for i in {0..5072}
 do 
@@ -11,15 +18,31 @@ do
   dateObs=${varArray[0]}
   numObs=${varArray[1]}
   filter=${varArray[2]}
+  filter=${filter/VISTA/VIRCAM}
+  band=${filter:7:1}
   tracts=${varArray[3]}
   filename=${varArray[4]}
+  visit=${varArray[5]}
   echo "Ingesting:"
+  echo $i
   echo $dateObs
   echo $numObs
   echo $filter
   echo $tracts
   echo $filename
-  ingestImages.py ../data $filename --ignore-ingested 
+  rm ./tmp/tmp_stack.fit
+  rm ./tmp/tmp_confidence.fit
+  cp $filename ./tmp/tmp_stack.fit
+  cp ${filename/st.fit/st_conf.fit} ./tmp/tmp_confidence.fit
+  #visit="$(python fitToExpNum.py ./tmp/tmp_stack.fit)"
+  echo $visit
+  
+  butler ingest-raws $repo $filename -t copy
+  butler ingest-files --formatter=lsst.obs.vista.VircamRawFormatter $repo confidence video ./tmp/confidence.ecsv --data-id exposure=$visit,band=$band,physical_filter=$filter -t copy
 done
-#processCcd.py ../data --rerun processCcdOutputs --id dateObs=$dateObs numObs=$numObs --clobber-config
-#makeCoaddTempExp.py ../data --rerun coadd --selectId dateObs=$dateObs numObs=$numObs filter=$filter --id filter=$filter tract=$tracts  --clobber-config
+
+#Define the visits from the ingested exposures
+butler define-visits $repo VIRCAM 
+#We don't have calibs but we need the collection for later processing
+butler write-curated-calibrations $repo VIRCAM
+
