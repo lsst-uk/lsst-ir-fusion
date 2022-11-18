9ba81b2444c5a71587e5e93a1329d2e29a1aab8b
Raphael Shirley <raphaelshirley@gmail.com>
2022-11-18 16:42:01 +0000

    remove some old gen2 scripts

--- dmu4/dmu4_Example/convert_repo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /Users/raphaelshirley/Documents/github/lsst_stack/loadLSST.bash
-setup lsst_distrib
-setup obs_vista 21.0.0-1
-
-butler convert --gen2root data_g2 data_g3_test
-
--- dmu4/dmu4_Example/makeSkyMap.py
+++
@@ -1,6 +0,0 @@
-config.name = "hscPdr2"
-config.skyMap.name = "rings"
-config.skyMap["rings"].numRings = 120
-config.skyMap["rings"].projection = "TAN"
-config.skyMap["rings"].tractOverlap = 1.0/60  # Overlap between tracts (degrees)
-config.skyMap["rings"].pixelScale = 0.168   # HSC=0.168 VISTA= 0.339
