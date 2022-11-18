95835236c584cfcc90911a13cc88e7a2821bdf64
Raphael Shirley <raphael.shirley@hotmail.co.uk>
2022-11-18 17:44:43 +0000

    Making HSC imports

--- dmu4/dmu4_Example/test_patch_gen3.sh
+++ dmu4/dmu4_Example/test_patch_gen3.sh
@@ -12,44 +12,67 @@ if [ -f $repo/butler.yaml ]; then
     rm -rf $repo
 fi
 
-if [ ! -f $repo/butler.yaml ]; then
-    #rm -r $repo
-    butler create $repo
-    #butler register-instrument $repo lsst.obs.subaru.HyperSuprimeCam
-    butler register-instrument $repo lsst.obs.vista.VIRCAM
-    #Make and register the all sky skymap using local config file
-    butler register-skymap $repo -C "$OBS_VISTA_DIR/config/makeSkyMap.py"
-    
-    # Import the reference catalogues to the butler.
-    butler register-dataset-type $repo ps1_pv3_3pi_20170110_vista SimpleCatalog htm7
-    cp -r ../../dmu2/data/video_gen3 $repo  # VIDEO ref
-    butler ingest-files -t direct $repo ps1_pv3_3pi_20170110_vista refcats data/video_gen3/filename_to_htm.ecsv 
-    
-    #Ingest the raw exposures _st for stacks [0-9] for exposures
-    butler ingest-raws $repo ../../dmu0/dmu0_VISTA/dmu0_VIDEO/data/*/*_st.fit -t copy --output-run VIRCAM/raw/video
 
-    #Define the visits from the ingested exposures
-    butler define-visits $repo VIRCAM --collections VIRCAM/raw/video
-    #We don't have calibs but we need the collection for later processing
-    butler write-curated-calibrations $repo VIRCAM
-fi
+#Create the Butler
+butler create $repo
+#Register VIRCAM
+butler register-instrument $repo lsst.obs.vista.VIRCAM
+#Make and register the all sky skymap using local config file
+butler register-skymap $repo -C "$OBS_VISTA_DIR/config/makeSkyMap.py"
+
+# Import the reference catalogues to the butler.
+butler register-dataset-type $repo ps1_pv3_3pi_20170110_vista SimpleCatalog htm7
+cp -r ../../dmu2/data/video_gen3 $repo  # VIDEO ref
+butler ingest-files -t copy $repo ps1_pv3_3pi_20170110_vista refcats/video \
+	data/video_gen3/filename_to_htm.ecsv 
+rm -r $repo/video_gen3
+
+#Ingest the raw exposures _st for stacks [0-9] for exposures
+butler ingest-raws $repo ../../dmu0/dmu0_VISTA/dmu0_VIDEO/data/*/*_st.fit \
+	-t copy --output-run VIRCAM/raw/video
+
+#Define the visits from the ingested exposures
+butler define-visits $repo VIRCAM --collections VIRCAM/raw/video
+#We don't have calibs but we need the collection for later processing
+butler write-curated-calibrations $repo VIRCAM
+
 
 #Import confidence maps
-butler register-dataset-type $repo confidence ExposureF instrument band physical_filter exposure detector
-butler ingest-files --formatter=lsst.obs.vista.VircamRawFormatter $repo confidence confidence/video ../../dmu0/dmu0_VISTA/dmu0_VIDEO/test_export_confidence.ecsv -t copy 
+butler register-dataset-type $repo \
+    confidence ExposureF instrument band physical_filter exposure detector
+butler ingest-files --formatter=lsst.obs.vista.VircamRawFormatter $repo \
+    confidence confidence/video \
+    ../../dmu0/dmu0_VISTA/dmu0_VIDEO/test_export_confidence.ecsv -t copy 
 
-#Run processCcd on some exposures singleFrame processCcd
-pipetask run -d "detector IN (9,10) AND band IN ('J','K')" -b $repo --input confidence/video,VIRCAM/raw/video,refcats,VIRCAM/calib --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#singleFrame" --output videoSingleFrame  
+#Run the singleFrame processing
+pipetask run -d "detector IN (9,10) AND band IN ('J','K')" \
+    -b $repo --input confidence/video,VIRCAM/raw/video,refcats/video,VIRCAM/calib \
+    --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#singleFrame" \ 
+    --output videoSingleFrame  
 
 #Coadd the exposures
-pipetask run -d "tract=8524 AND patch IN (39,48) AND skymap='hscPdr2' " -b $repo --input confidence/video,skymaps,VIRCAM/raw/video,refcats,VIRCAM/calib,videoSingleFrame  --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#coaddDetect" --output videoCoaddDetect
+pipetask run -d "tract=8524 AND patch IN (39,48) AND skymap='hscPdr2' " \
+    -b $repo \
+    --input videoSingleFrame \
+    --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#coaddDetect" \
+    --output videoCoaddDetect
 
 #Import HSC deepCoadd images and detections
-export coaddRun=hscImports
-butler ingest-files --formatter=lsst.obs.base.formatters.fitsExposure.FitsExposureFormatter $repo deepCoadd $coaddRun ../../dmu0/dmu0_HSC/data/calexp_2.ecsv
-butler ingest-files --formatter=lsst.obs.base.formatters.fitsExposure.FitsExposureFormatter $repo deepCoadd_calexp $coaddRun ../../dmu0/dmu0_HSC/data/calexp_2.ecsv
-butler ingest-files --formatter=lsst.obs.base.formatters.fitsGeneric.FitsGenericFormatter $repo deepCoadd_det $coaddRun ../../dmu0/dmu0_HSC/data/det_2.ecsv
+export coaddRun=hsc/dud
+butler ingest-files \
+    --formatter=lsst.obs.base.formatters.fitsExposure.FitsExposureFormatter $repo \
+    deepCoadd $coaddRun ../../dmu0/dmu0_HSC/data/calexp_2.ecsv
+butler ingest-files \
+    --formatter=lsst.obs.base.formatters.fitsExposure.FitsExposureFormatter $repo \
+    deepCoadd_calexp $coaddRun ../../dmu0/dmu0_HSC/data/calexp_2.ecsv
+butler ingest-files \
+    --formatter=lsst.obs.base.formatters.fitsGeneric.FitsGenericFormatter $repo \
+    deepCoadd_det $coaddRun ../../dmu0/dmu0_HSC/data/det_2.ecsv
 
 #Run photometry pipeline
-pipetask run -d "tract=8524 AND patch IN (39,48) AND skymap='hscPdr2' " -b $repo --input confidence/video,VIRCAM/raw/video,refcats,VIRCAM/calib,skymaps,videoSingleFrame,videoCoaddDetect,$coaddRun --register-dataset-types -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#multiVisitLater" --output videoMultiVisitLater
+pipetask run -d "tract=8524 AND patch IN (39,48) AND skymap='hscPdr2' " -b $repo \
+    --input videoCoaddDetect,$coaddRun \
+    --register-dataset-types \
+    -p "$OBS_VISTA_DIR/pipelines/DRP_full.yaml#multiVisitLater" \
+    --output videoMultiVisitLater
 
