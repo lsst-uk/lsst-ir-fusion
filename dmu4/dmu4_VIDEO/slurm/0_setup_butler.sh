90067808c4cd294da9c4e4c2a088095aef177c48
Elham Saremi <85132726+Saremi-Elham@users.noreply.github.com>
2024-01-30 15:57:26 +0000

    Update setting up the butler

--- dmu4/dmu4_VIDEO/slurm/0_setup_butler.sh
+++ dmu4/dmu4_VIDEO/slurm/0_setup_butler.sh
@@ -13,9 +13,9 @@ if [ ! -f $repo/butler.yaml ]; then
     #rm -r $repo
     butler create $repo
     #butler register-instrument $repo lsst.obs.subaru.HyperSuprimeCam
-    butler register-instrument $repo lsst.obs.vista.VIRCAM
+    butler register-instrument $repo lsstuk.obs.vista.VIRCAM
     #Make and register the all sky skymap using local config file
-    butler register-skymap $repo -C ../../dmu4_Example/makeSkyMap.py
+    butler register-skymap $repo -C "$OBS_VISTA_DIR/config/makeSkyMap.py"
     
     # Import the reference catalogues to the butler.
     butler register-dataset-type $repo ps1_pv3_3pi_20170110_vista SimpleCatalog htm7
