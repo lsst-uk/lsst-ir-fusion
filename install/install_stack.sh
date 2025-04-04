0ecc754be066f8154ccacbef7bb53d3cc0a1c817
Elham Saremi <85132726+Saremi-Elham@users.noreply.github.com>
2025-04-04 13:25:09 +0100

    Update install_stack.sh

--- install/install_stack.sh
+++ install/install_stack.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Get latest weekly from https://github.com/lsst/lsst/tags
-export weekly='w.2024.15'
+# Get the latest weekly or official release tag from https://github.com/lsst/lsst/tags
+export weekly='w.2025.14'
+# export release='v28.0.2'
+
 unset LSST_HOME EUPS_PATH LSST_DEVEL EUPS_PKGROOT REPOSITORY_PATH
 
 mkdir -p source/$weekly
 cd source/$weekly
 
-# Download and run of "newinstall.sh"
+# Download and run of "newinstall.sh" 
+# For official release, we use 28.0.2 not v28.0.2 for example instead of $weekly.
 curl -OL https://raw.githubusercontent.com/lsst/lsst/$weekly/scripts/newinstall.sh
 bash newinstall.sh -ctb
 
@@ -15,7 +18,8 @@ bash newinstall.sh -ctb
 source loadLSST.bash
 
 # Install Science Pipelines packages
-eups distrib install -t w_2024_15 lsst_distrib
+# For official release, we use v28_0_2, for example.
+eups distrib install -t w_2025_14 lsst_distrib
 curl -sSL https://raw.githubusercontent.com/lsst/shebangtron/main/shebangtron | python
 
 # Install "obs_vista" package
