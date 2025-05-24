faef14820b0ba671e0a99effa0c1b2205d4f9952
Elham Saremi <85132726+Saremi-Elham@users.noreply.github.com>
2025-05-24 21:22:04 +0100

    Update setup.sh

--- dmu4/dmu4_VIDEO/slurm/setup.sh
+++ dmu4/dmu4_VIDEO/slurm/setup.sh
@@ -4,10 +4,12 @@
 
 # The weekly tag should be revised based on the installed stack
 # version.
-export weekly='w.2025.14'
+#export weekly='w.2025.14'
+export release='v29.0.1'
 
 # Load the LSST software environment into the shell
-source ../../../install/source/${weekly}/loadLSST.bash
+# source ../../../install/source/${weekly}/loadLSST.bash
+source ../../../install/source/${release}/loadLSST.bash
 
 # Setup Science Pipelines packages
 setup lsst_distrib
@@ -16,7 +18,7 @@ setup lsst_distrib
 setup obs_vista
 
 # Setup of wq_env for using ctrl_bps and the Parsl-based plug-in
-export wq_env=../../../install/source/${weekly}/stack/wq_env
+export wq_env=../../../install/source/${release}/stack/wq_env
 conda activate --stack ${wq_env}
 export PYTHONPATH=${wq_env}/lib/python3.13/site-packages:${PYTHONPATH}
 export PATH=${wq_env}/bin:${PATH}
