a951edd5d530d9c2262792ffaebaff2b4e8f9b78
Elham Saremi <saremi_elham@yahoo.com>
2025-08-20 22:30:40 +0100

    Ingesting raw images and confidence maps
    Until now, I had created a local Butler, built a ComCam skymap, and
    ingested a reference catalog.
    
    In this commit, I extend the workflow to also ingest raw images and
    their associated confidence maps.
    
    To do this, I selected a sample set of images and placed them under
    dmu0/dmu0_VISTA/dmu0_VIDEO_CSDF/data. I then wrote a Jupyter notebook
    that generates the required confidence map metadata (exposure ID,
    filter, detector, etc.) to enable ingestion into Butler.

--- dmu4/dmu4_DP1/dmu4_DP1_Example/test_patch_gen3.sh
+++ dmu4/dmu4_DP1/dmu4_DP1_Example/test_patch_gen3.sh
@@ -23,15 +23,37 @@ fi
 #Create the Butler
 butler create $repo
 
+
 #Register VIRCAM
 butler register-instrument $repo lsstuk.obs.vista.VIRCAM
 
+
 #Make and register the all sky skymap using local config file
 butler register-skymap $repo -C "$OBS_VISTA_DIR/config/comCam/makeSkyMap.py"
+
 
 # Import the reference catalogues to the butler.
 butler register-dataset-type $repo the_monster_20250219_vista SimpleCatalog htm7
 cp -r ../../../dmu2/dmu2_DP1/data/video_cdfs $repo
 butler ingest-files -t copy $repo the_monster_20250219_vista refcats/video \
 	data/video_cdfs/filename_to_htm.ecsv
-rm -r $repo/video_cdfs 
+rm -r $repo/video_cdfs
+
+
+#Ingest the raw exposures _st for stacks [0-9] for exposures
+butler ingest-raws $repo ../../../dmu0/dmu0_VISTA/dmu0_VIDEO_CSDF/data/*/*_st.fit \
+	-t copy --output-run VIRCAM/raw/video
+
+
+#Define the visits from the ingested exposures
+butler define-visits $repo VIRCAM --collections VIRCAM/raw/video
+#We don't have calibs but we need the collection for later processing
+butler write-curated-calibrations $repo VIRCAM video
+
+
+#Import confidence maps
+butler register-dataset-type $repo \
+    confidence ExposureF instrument band physical_filter exposure detector
+butler ingest-files --formatter=lsstuk.obs.vista.VircamRawFormatter $repo \
+    confidence confidence/video \
+    ../../../dmu0/dmu0_VISTA/dmu0_VIDEO_CSDF/example_export_confidence.ecsv -t copy
