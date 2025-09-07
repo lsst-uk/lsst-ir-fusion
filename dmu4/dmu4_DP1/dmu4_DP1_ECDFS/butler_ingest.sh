4696e4c1f3c5c30d3425cd90f42776300d20cb7b
Elham Saremi <saremi_elham@yahoo.com>
2025-09-07 15:32:27 +0100

    Add lsst_cells_v1 skymap and the Monster reference catalogue

---
+++ dmu4/dmu4_DP1/dmu4_DP1_ECDFS/butler_ingest.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Setup LSST Science pipeline environment; The weekly tag within the
+# "setup.sh" file should be revised based on the installed stack
+# version.
+source setup.sh
+
+
+# Set location of Butler
+export repo=../data
+
+
+# Set an environment variable to decide which config profile to use
+export OBS_VISTA_PROFILE=ComCam
+
+
+# Delete old butler if already present
+if [ -f $repo/butler.yaml ]; then
+    rm -r $repo
+fi
+
+
+# Create the Butler
+butler create $repo
+
+
+# Register VIRCAM
+butler register-instrument $repo lsstuk.obs.vista.VIRCAM
+
+
+# Make and register the all sky skymap using local config file
+butler register-skymap $repo -C \
+       "$OBS_VISTA_DIR/config/ComCam/makeSkyMap.py"
+
+
+# Import the reference catalogues to the butler
+butler register-dataset-type $repo \
+       the_monster_20250219_vista SimpleCatalog htm7
+cp -r ../../../dmu2/dmu2_DP1/data/video_cdfs $repo
+cd $repo
+cd ..
+butler ingest-files -t copy data \
+       the_monster_20250219_vista refcats/video \
+       data/video_cdfs/filename_to_htm.ecsv
+rm -r data/video_cdfs
+cd dmu4_DP1_ECDFS
