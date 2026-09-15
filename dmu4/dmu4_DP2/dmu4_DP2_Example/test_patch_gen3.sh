de81db1b67ebc15e74e0eca65c195d9c11d820ef
Elham Saremi <85132726+Saremi-Elham@users.noreply.github.com>
2026-09-15 17:57:50 +0100

    Ingest VIRCAM raws, visits, calibrations, and confidence maps
    Until now, I set up the DP2 Gen3 Butler and registered the two
    ELAIS reference-catalogue approaches.
    
    In this commit, I extend the test-patch setup to ingest the selected
    VIDEO VIRCAM raw exposures, define visits, register the curated
    calibration collection required by later processing, and register and
    ingest the corresponding confidence maps into the Butler.

--- dmu4/dmu4_DP2/dmu4_DP2_Example/test_patch_gen3.sh
+++ dmu4/dmu4_DP2/dmu4_DP2_Example/test_patch_gen3.sh
@@ -62,3 +62,22 @@ butler ingest-files -t copy $repo \
     refcats/video_elais_ap2 \
     data/video_elais_ap2/filename_to_htm.ecsv
 rm -r $repo/video_elais_ap2
+
+
+# Ingest the raw exposures _st for stacks [0-9] for exposures
+butler ingest-raws $repo ../../../dmu0/dmu0_VISTA/dmu0_VIDEO_ELAIS/data/*_st.fit \
+	-t copy --output-run VIRCAM/raw/video_elais
+
+
+# Define the visits from the ingested exposures
+butler define-visits $repo VIRCAM --collections VIRCAM/raw/video_elais
+# We don't have calibs but we need the collection for later processing
+butler write-curated-calibrations $repo VIRCAM video_elais
+
+
+# Import confidence maps
+butler register-dataset-type $repo \
+    confidence ExposureF instrument band physical_filter exposure detector
+butler ingest-files --formatter=lsstuk.obs.vista.VircamRawFormatter $repo \
+    confidence confidence/video_elais \
+    ../../../dmu0/dmu0_VISTA/dmu0_VIDEO_ELAIS/example_export_confidence.ecsv -t copy
